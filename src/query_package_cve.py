0a4c40b4733f45f8269d7a3b9870370d7dbc2de1
aaxhag <aaxhag@kth.se>
2026-05-15 13:50:23 +0200

    improvement to querying

--- src/query_package_cve.py
+++ src/query_package_cve.py
@@ -7,25 +7,44 @@
 # EXAMPLE USAGE OF FUNCTIONS:
 
 # query_package_for_cve('golang.org/x/text', '0.3.5')
-# version_is_not_patched({ 'id': 'GO-2024-3038', 'modified': '2024-08-06T22:03:16Z', 'fixed': '0.7.1' }, '0.3.5')
+# version_is_not_patched({ 'id': 'GO-2024-3038', 'modified': '2024-08-06T22:03:16Z', 'fixed': '0.7.1' }, golang.org/x/text, '0.3.5')
 
 from requests import get
-from packaging import version
+from re import search
+
+import semver
 import logging
 
+PATH        = 'path'
+VULNS       = 'vulns'
+FIXED       = 'fixed'
+ID          = 'id'
+AFFECTED    = 'affected'
+PACKAGE     = 'package'
+NAME        = 'name'
+RANGES      = 'RANGES'
+EVENTS      = 'EVENTS'
+
+# Different types 
+INTRODUCED      = 'introduced'
+FIXED           = 'fixed'
+LAST_AFFECTED   = 'last_affected'
+LIMIT           = 'limit'
+
 PACKAGE_CVE_JSON = get('https://vuln.go.dev/index/modules.json').json()
+
 # If a version patch number is missing, or is an unrecognized format, default to this
 DEFAULT_TO_VULNERABLE = False
 PACKAGE_CVE_MISSING_PATCH_VERSION = f'''A vulnerability is missing a patch version or has an unrecognized version format. 
         The vulnerability could still exist, defaulting to {'NOT ' if not DEFAULT_TO_VULNERABLE else ''}VULNERABLE!'''
 
-PATH = 'path'
-VULNS = 'vulns'
-FIXED = 'fixed'
-ID = 'id'
 
 
 # Returns a list of CVEs, if it's empty, then there were no CVEs to be found
+
+# ARGUMENTS: 
+# package_name: str     -- Name of the possibly affected package
+# package_version: str  -- Version of the possibly affected package
 def query_package_for_cve(package_name: str, package_version: str) -> list:
     entry_returned = filter(
                         lambda entry: package_name == entry.get(PATH),
@@ -37,8 +56,8 @@ def query_package_for_cve(package_name: str, package_version: str) -> list:
     except: return []
 
     unpatched_vulns = filter(
-                        lambda vuln: version_is_not_patched(vuln, package_version), 
-                        entry_returned[VULNS]
+                        lambda vuln: version_is_not_patched(vuln, package_name, package_version), 
+                        entry_returned.get(VULNS)
                     )
     
     return list(unpatched_vulns)
@@ -47,13 +66,42 @@ def query_package_for_cve(package_name: str, package_version: str) -> list:
 # In the case that the version is missing or is using an unrecognized format,
 # we might consider it still vulnerable, it depends on DEFAULT_TO_VULNERABLE. 
 # However, we do display a warning either way
-def version_is_not_patched(vulnerability: dict, package_version: str) -> bool:
-    patched_version = vulnerability.get(FIXED)
-    cve_identifier = vulnerability.get(ID)
 
-    try: 
-        package_version = version.parse(package_version)
-        patched_version = version.parse(patched_version)
+# ARGUMENTS: 
+# vulnerability: dict   -- The dictionary of the vulnerability
+# package_name: str     -- Name of the possibly affected package
+# package_version: str  -- Version of the possibly affected package
+def version_is_not_patched(vulnerability: dict, package_name: str, package_version: str) -> bool:
+    cve_identifier = vulnerability.get(ID)
+    cve_info = get(f'https://vuln.go.dev/ID/{cve_identifier}.json').json()
+
+    try:
+        for affected in cve_info.get(AFFECTED):
+            if affected.get(PACKAGE).get(NAME) != package_name:
+                continue
+
+            vulnerable = False
+            # I <3 nesting
+            for range in affected.get(RANGES):
+                for event in range.get(EVENTS):
+                    if (event.get(INTRODUCED) is not None):
+                        introduced_version = event.get(INTRODUCED)
+
+                        semver.compare(package_version, introduced_version)
+                    elif (event.get(FIXED) is not None):
+                        pass
+                    elif (event.get(LAST_AFFECTED) is not None):
+                        pass
+                    elif (event.get(LIMIT) is not None):
+                        pass
+                    else:
+                        pass
+
+
+
+        semver.compare(package_version, patched_version)
+        
+
     except:
         warning_msg = PACKAGE_CVE_MISSING_PATCH_VERSION
         if (cve_identifier is not None): warning_msg += f' ({cve_identifier})'
